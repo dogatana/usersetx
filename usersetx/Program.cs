c635a3578e300690cf3ec594390edeaecfe74400
Toshihiko Ichida <dogatana@gmail.com>
2022-02-10 11:00:52 +0900

    ヘルプ追加、 name=value の形式を追加

--- usersetx/Program.cs
+++ usersetx/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace usersetx
 {
@@ -7,17 +8,38 @@ namespace usersetx
         static void Main(string[] args)
         {
             if (args.Length == 1) {
-                Console.WriteLine($"unset {args[0]}");
-                SetVariable(args[0], null);
+                var arg = args[0];
+                if (arg == "-h" || arg == "--help") {
+                    ShowUsage();
+                } else if (args[0].IndexOf('=') != -1) {
+                    var key_values = arg.Split('=').Select(v => v.Trim()).ToArray();
+                    SetVariable(key_values[0], key_values[1]);
+                } else {
+                    UnsetVariable(arg);
+                }
             } else if (args.Length == 2) {
-                Console.WriteLine($"set {args[0]}={args[1]}");
                 SetVariable(args[0], args[1]);
             }
         }
 
         static void SetVariable(string name, string value)
         {
-            Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.User);
+            //Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.User);
+            Console.WriteLine($"set {name}={value}");
+        }
+        static void UnsetVariable(string name)
+        {
+            //Environment.SetEnvironmentVariable(name, null, EnvironmentVariableTarget.User);
+            Console.WriteLine($"unset {name}");
+        }
+
+        static void ShowUsage()
+        {
+            var message = @"usage: usersetx args
+    usersetx name value - set value to name
+    usersetx name=value - set value to name
+    usersetx name       - delete nanme";
+            Console.WriteLine(message);
         }
     }
 }
